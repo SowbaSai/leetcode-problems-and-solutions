e09ad0a5268ed2581389b0b8d90995e64c2bcfa3
SOWBARNIKA V <153999520+SowbaSai@users.noreply.github.com>
2024-08-04 14:57:18 +0530

    [1768] [Time Beats: 100.00%] [Memory Beats: 39.76%] - LeetPush

--- 1768-Merge-Strings-Alternately.cpp
+++ 1768-Merge-Strings-Alternately.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int i=0;
-        string result="";
-        while(i<word1.size()||i<word2.size()){
-            if(i<word1.size()){
-                result+=word1[i];
+        string result = "";
+        int i = 0;
+        while (i < word1.length() || i < word2.length()) {
+            if (i < word1.length()) {
+                result += word1[i];
             }
-            if(i<word2.size()){
-                result+=word2[i];
+            if (i < word2.length()) {
+                result += word2[i];
             }
             i++;
         }
-    return result;
+        return result;
     }
 };
